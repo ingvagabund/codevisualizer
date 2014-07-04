9fdc61412a6681dbc40e6dbed92c8e343bcb13d2
Jan Chaloupka <jchaloup@redhat.com>
2014-07-04 08:03:01 +0200

    Adding options and destination directory

--- visualize.py
+++ visualize.py
@@ -23,18 +23,51 @@
 
 import sys
 import os
+import optparse
 from CodeParser import CodeParser
 from VisualizationParser import VisualizationParser
 from HTMLVisualizer import HTMLVisualizer
 
 version = "0.0"
-debug_level = 1
+debug_level = 0
 
-src_file = sys.argv[1]
+# argument parsing
+parser = optparse.OptionParser(usage="usage: %prog [--debug] [--dest=DEST] file")
+parser.add_option(
+    "", "--debug", dest = "debug", action = "store_true", default = False,
+    help = "debug mode"
+)
+
+parser.add_option(
+    "", "--dest", dest = "dest", action = "store", default = "examples",
+    help = "destination folder for generated files"
+)
+
+options, args = parser.parse_args()
+
+if options.debug:
+	debug_level = 1
+
+if len(args) != 1:
+	print "Input source code missing!!!"
+	exit(0)
+
+if len(options.dest) == 0:
+	print "Destination directory name must be at least of length 1"
+	exit(0)
+
+src_file = args[0]
+destination = options.dest
 
 def debug(msg):
 	if debug_level > 0:
 		print msg
+
+def mkdir_p(path):
+    if not os.access(path, os.F_OK):
+        os.makedirs(path)
+
+#####################################################################
 
 debug("Code Visualizer, version %s" % version)
 debug("Opening file: %s" % src_file)
@@ -45,8 +78,19 @@ with open(src_file, "r") as file:
 	src_content = file.read()
 
 # create visualization file
+# is the destination relative?
+if destination[0] != "/":
+	destination = "%s/%s" % (os.getcwd(), destination)
+
+# canonize the path
+destination = os.path.realpath(destination)
+
+# destination folder exists?
+if not os.path.exists(destination):
+	mkdir_p(destination)
+
 # extract basename
-vis_file = "examples/" + os.path.basename(src_file) + ".vis"
+vis_file = destination + "/" + os.path.basename(src_file) + ".vis"
 
 # read visualization file
 debug("Opening visualization file: %s " % vis_file)
@@ -56,7 +100,7 @@ if not os.path.exists(vis_file):
 	open(vis_file, 'a').close()
 
 debug("Opening html file for write: %s " % vis_file)
-html_file = "examples/" + os.path.basename(src_file) + ".html"
+html_file = destination + "/" + os.path.basename(src_file) + ".html"
 # does the html file exist?
 if not os.path.exists(html_file):
 	debug("file not found, creating empty file")
